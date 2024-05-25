1ba646206e2e58feaece2c0d1fcab58092b2ff53
LyQing63 <1827304875@qq.com>
2024-05-26 00:26:45 +0800

    add logo

---
+++ src/api/models/models.ts
@@ -0,0 +1,48 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type Page= {
+    /**
+     * content
+     */
+    content?: string;
+    /**
+     * createDate
+     */
+    createDate?: string;
+    /**
+     * id
+     */
+    id?: string;
+    /**
+     * isDelete
+     */
+    isDelete?: string;
+    /**
+     * isInBin
+     */
+    isInBin?: string;
+    /**
+     * updateDate
+     */
+    updateDate?: string;
+    /**
+     * userId
+     */
+    userId?: number;
+};
+
+export type User = {
+    createTime: string,
+    gender: number|undefined,
+    id: number|undefined,
+    isVip: number|undefined,
+    updateTime: string,
+    userAccount: string,
+    userAvatar: string,
+    userName: string,
+    userPassword: string,
+    userRole: string
+};
+

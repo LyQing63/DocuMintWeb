daa4e872f0fb98ae9deaed0efe449c555b59e342
LyQing63 <1827304875@qq.com>
2024-06-11 21:43:01 +0800

    fix build bugs

--- src/components/tailwind/generative/ai-selector.tsx
+++ src/components/tailwind/generative/ai-selector.tsx
@@ -28,7 +28,7 @@ export function AISelector({ onOpenChange }: AISelectorProps) {
 
   const { completion, complete, isLoading } = useCompletion({
     // id: "novel",
-    api: "https://lyqing.icu/ai/generate",
+    api: "https://fez7cag5adt5q0h5.aistudio-hub.baidu.com/generate",
     onResponse: (response) => {
       if (response.status === 429) {
         toast.error("You have reached your request limit for the day.");
